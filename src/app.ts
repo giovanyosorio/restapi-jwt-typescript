156ac788def337c21f06c393107c2bd76c6cbbe9
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-06 22:20:27 +0000

    routes app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express"
-
+import authRouter from "./routes/auth"
+import morgan from "morgan"
 const app=express()
-
+//middlewares
 app.use(express.json())
+app.use(morgan("dev"))
+//Settings
 app.set("PORT",3000)
-
+//routes
+app.use(authRouter)
 
 export default app
