8889bd425b889d750629ff9d4b707b0ee285b458
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-07 20:59:04 +0000

    using bcrypt to encrypt the password

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from 'express';
 import {pool} from "../routes/database"
 import jwt from "jsonwebtoken"
-import { OkPacket } from 'mysql2';
+import { OkPacket } from 'mysql';
+import bcrypt from "bcryptjs"
 
 export const signup =  async (req:Request,res:Response)=>{
     try {
-        const { username, email, password } = req.body;
-        const result = await pool.query("INSERT INTO users (username,email,password) VALUES (?,?,?)", [username, email, password]);
-        const { insertId } = result[0]; // Extract insertId from the first element of the result array
-        res.send("signup");
+        const {username, email, password} = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const [result] = await pool.query("INSERT INTO users (username,email,password) VALUES (?,?,?)", [username, email, hashPassword]);
+        const insertId = (result as any).insertId; // Use this line if TypeScript complains
         console.log({ result });
         //jwt
-        const token = jwt.sign({ id: insertId }, process.env.TOKEN_SECRET || "tokentest");
-        res.json(token);
+        const token = jwt.sign({id: insertId}, process.env.TOKEN_SECRET || "tokentest");
+        res.header("auth-token", token).json({message: "User created", token});
     } catch (error) {
         res.status(500).send("error");
     }
