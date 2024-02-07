8053365cb2f6ce2d4abebfcb49d8c3b518486903
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-07 19:35:04 +0000

    routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,9 @@
 import {Router} from 'express'
-
+import {signin,signup,profile} from "./../controllers/auth.controller"
 const router:Router=Router()
 
+router.post("/api/signup",signup)
+router.post("/api/signin",signin)
+router.get("/api/profile",profile)
 
-router.get("/",(req,res)=>{
-    res.send("hello world")
-})
 export default router
