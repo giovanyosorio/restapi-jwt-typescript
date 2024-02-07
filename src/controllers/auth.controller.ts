eea5fbd9f28dfe00ec21b05f45ad9d12d7cf89a1
Giovany Osorio <30839218+giovanyosorio@users.noreply.github.com>
2024-02-07 21:16:57 +0000

    signin

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,6 +3,7 @@ import {pool} from "../routes/database"
 import jwt from "jsonwebtoken"
 import { OkPacket } from 'mysql';
 import bcrypt from "bcryptjs"
+import { RowDataPacket } from 'mysql2';
 
 export const signup =  async (req:Request,res:Response)=>{
     try {
@@ -17,13 +18,30 @@ export const signup =  async (req:Request,res:Response)=>{
         const token = jwt.sign({id: insertId}, process.env.TOKEN_SECRET || "tokentest");
         res.header("auth-token", token).json({message: "User created", token});
     } catch (error) {
-        res.status(500).send("error");
+        res.status(500).send(error);
     }
 
 }
 
-export const signin = (req:Request,res:Response)=>{
-    res.send("signin")
+export const signin = async (req:Request,res:Response)=>{
+
+    const { email, password } = req.body;
+    // Perform the query and explicitly type the result
+    const [rows] = await pool.query<RowDataPacket[]>("SELECT * FROM users WHERE email = ?", [email]);
+    
+    // Now that we've asserted rows is RowDataPacket[], accessing rows[0] is valid
+    const user = rows[0];
+    if (!user) return res.status(400).json({ message: "Email or password is wrong" });
+    
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) return res.status(400).json({ message: "Invalid password" });
+    
+    const token = jwt.sign({ id: user.id }, process.env.TOKEN_SECRET || "tokentest", {
+        expiresIn: 60 * 60 * 24 // 1 day
+    });
+    res.header("auth-token", token).json({ message: "Welcome back", token });
+    
+
 }
 
 export const profile = (req:Request,res:Response)=>{
